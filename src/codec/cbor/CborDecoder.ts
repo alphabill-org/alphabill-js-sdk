769bb82ea2efefc269ebf44bff130745e2729f77
Martti Marran <martti.marran@guardtime.com>
2024-11-26 14:12:09 +0200

    Add readRawCbor to cbor decoder

--- src/codec/cbor/CborDecoder.ts
+++ src/codec/cbor/CborDecoder.ts
@@ -9,157 +9,121 @@ export class CborDecoder {
     this.data = new Uint8Array(data);
   }
 
-  public decodeUnknownCbor(): unknown {
+  public readUnsignedInteger(): bigint {
+    const initialByte = this.data[this.position];
+    const majorType = initialByte & BitMask.MAJOR_TYPE;
+    if (majorType != MajorType.UNSIGNED_INTEGER) {
+      throw new Error('Major type mismatch, expected unsigned integer.');
+    }
+    return this.readLength(majorType);
+  }
+
+  public readNegativeInteger(): bigint {
+    throw new Error('Not implemented.');
+  }
+
+  public readByteString(): Uint8Array {
+    const initialByte = this.data[this.position];
+    const majorType = initialByte & BitMask.MAJOR_TYPE;
+    if (majorType != MajorType.BYTE_STRING) {
+      throw new Error('Major type mismatch, expected byte string.');
+    }
+    const length = Number(this.readLength(majorType));
+    return this.data.subarray(this.position, this.position + length);
+  }
+
+  public readTextString(): string {
+    const initialByte = this.data[this.position];
+    const majorType = initialByte & BitMask.MAJOR_TYPE;
+    if (majorType != MajorType.TEXT_STRING) {
+      throw new Error('Major type mismatch, expected text string.');
+    }
+    const length = Number(this.readLength(majorType));
+    return new TextDecoder().decode(this.data.subarray(this.position, this.position + length));
+  }
+
+  public readArray(): Uint8Array[] {
+    const initialByte = this.data[this.position];
+    const majorType = initialByte & BitMask.MAJOR_TYPE;
+    if (majorType != MajorType.ARRAY) {
+      throw new Error('Major type mismatch, expected array.');
+    }
+    const length = this.readLength(majorType);
+    const result: Uint8Array[] = [];
+    for (let i = 0; i < length; i++) {
+      result.push(this.readRawCbor());
+    }
+
+    return result;
+  }
+
+  public readMap(): Map<unknown, unknown> {
+    throw new Error('Not implemented.');
+  }
+
+  public readTag(): [bigint, Uint8Array] {
+    const initialByte = this.data[this.position];
+    const majorType = initialByte & BitMask.MAJOR_TYPE;
+    if (majorType != MajorType.TAG) {
+      throw new Error('Major type mismatch, expected tag.');
+    }
+    const tag = this.readLength(majorType);
+    return [tag, this.readRawCbor()];
+  }
+
+  private readLength(majorType: number): bigint {
+    const additionalInformation = this.data[this.position++] & BitMask.ADDITIONAL_INFORMATION;
+    if (additionalInformation < 24) {
+      return BigInt(additionalInformation);
+    }
+    switch (majorType) {
+      case MajorType.ARRAY:
+      case MajorType.BYTE_STRING:
+      case MajorType.TEXT_STRING:
+        if (additionalInformation == 31) {
+          throw new Error('Indefinite length array not supported.');
+        }
+    }
+
+    if (additionalInformation > 27) {
+      throw new Error('Encoded item is not well-formed.');
+    }
+    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
+    let t = BigInt(0);
+    for (let i = 0; i < numberOfLengthBytes; ++i) {
+      t = (t << 8n) | BigInt(this.data[this.position++] & 0xff);
+    }
+    return t;
+  }
+
+  private readRawCbor(): Uint8Array {
+    const offset = this.position;
     const majorType = this.data[this.position] & BitMask.MAJOR_TYPE;
+    const length = this.readLength(majorType);
+
     switch (majorType) {
       case MajorType.UNSIGNED_INTEGER:
-        return this.decodeUnsignedInteger();
+        return this.data.subarray(offset, this.position);
       case MajorType.NEGATIVE_INTEGER:
         throw new Error('Decoding negative integers not supported.');
       case MajorType.BYTE_STRING:
-        return this.decodeByteString();
       case MajorType.TEXT_STRING:
-        return this.decodeTextString();
+        this.position += Number(length);
+        return this.data.subarray(offset, this.position);
       case MajorType.ARRAY:
-        return this.decodeArray();
+        for (let i = 0; i < Number(length); i++) {
+          this.readRawCbor();
+        }
+        return this.data.subarray(offset, this.position);
       case MajorType.MAP:
         throw new Error('Decoding map not supported.');
       case MajorType.TAG:
-        return this.decodeTag();
+        this.readRawCbor();
+        return this.data.subarray(offset, this.position);
       case MajorType.FLOAT_SIMPLE_BREAK:
         throw new Error('Decoding float, simple value or break not supported.');
       default:
         throw new Error('Unknown major type.');
     }
-  }
-
-  public decodeUnsignedInteger(): bigint {
-    const initialByte = this.data[this.position++];
-    const majorType = initialByte & BitMask.MAJOR_TYPE;
-    if (majorType != MajorType.UNSIGNED_INTEGER) {
-      throw new Error('Major type mismatch, expected unsigned integer.');
-    }
-    const additionalInformation = initialByte & BitMask.ADDITIONAL_INFORMATION;
-    if (additionalInformation < 24) {
-      return BigInt(additionalInformation);
-    }
-    if (additionalInformation > 27) {
-      throw new Error('Encoded item is not well-formed.');
-    }
-    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
-    return this.decodeUint(numberOfLengthBytes);
-  }
-
-  public decodeNegativeInteger(): bigint {
-    throw new Error('Not implemented.');
-  }
-
-  public decodeByteString(): Uint8Array {
-    const initialByte = this.data[this.position++];
-    const majorType = initialByte & BitMask.MAJOR_TYPE;
-    if (majorType != MajorType.BYTE_STRING) {
-      throw new Error('Major type mismatch, expected byte string.');
-    }
-    const additionalInformation = initialByte & BitMask.ADDITIONAL_INFORMATION;
-    if (additionalInformation < 24) {
-      const startPosition = this.position;
-      const endPosition = startPosition + additionalInformation;
-      this.position = endPosition;
-      return this.data.subarray(startPosition, endPosition);
-    }
-    if (additionalInformation == 31) {
-      throw new Error('Indefinite length byte string not supported.');
-    }
-    if (additionalInformation > 27) {
-      throw new Error('Encoded item is not well-formed');
-    }
-    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
-    const textLength = this.decodeUint(numberOfLengthBytes);
-    const startPosition = this.position;
-    const endPosition = startPosition + Number(textLength);
-    this.position = endPosition;
-    return this.data.subarray(startPosition, endPosition);
-  }
-
-  public decodeTextString(): string {
-    const initialByte = this.data[this.position++];
-    const majorType = initialByte & BitMask.MAJOR_TYPE;
-    if (majorType != MajorType.TEXT_STRING) {
-      throw new Error('Major type mismatch, expected text string.');
-    }
-    const additionalInformation = initialByte & BitMask.ADDITIONAL_INFORMATION;
-    if (additionalInformation < 24) {
-      const startPosition = this.position;
-      const endPosition = startPosition + additionalInformation;
-      this.position = endPosition;
-      return new TextDecoder().decode(this.data.subarray(startPosition, endPosition));
-    }
-    if (additionalInformation == 31) {
-      throw new Error('Indefinite length text string not supported.');
-    }
-    if (additionalInformation > 27) {
-      throw new Error('Encoded item is not well-formed.');
-    }
-    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
-    const textLength = this.decodeUint(numberOfLengthBytes);
-    const startPosition = this.position;
-    const endPosition = startPosition + Number(textLength);
-    this.position = endPosition;
-    const textStringBytes = this.data.subarray(startPosition, endPosition);
-    return new TextDecoder().decode(textStringBytes);
-  }
-
-  public decodeArray(): unknown[] {
-    const initialByte = this.data[this.position++];
-    const majorType = initialByte & BitMask.MAJOR_TYPE;
-    if (majorType != MajorType.ARRAY) {
-      throw new Error('Major type mismatch, expected array.');
-    }
-    const result: unknown[] = [];
-    const additionalInformation = initialByte & BitMask.ADDITIONAL_INFORMATION;
-    if (additionalInformation < 24) {
-      for (let i = 0; i < additionalInformation; i++) {
-        result.push(this.decodeUnknownCbor());
-      }
-    }
-    if (additionalInformation == 31) {
-      throw new Error('Indefinite length array not supported.');
-    }
-    if (additionalInformation > 27) {
-      throw new Error('Encoded item is not well-formed.');
-    }
-    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
-    const arrayLength = this.decodeUint(numberOfLengthBytes);
-    for (let i = 0; i < arrayLength; i++) {
-      result.push(this.decodeUnknownCbor());
-    }
-    return result;
-  }
-
-  public decodeMap(): Map<unknown, unknown> {
-    throw new Error('Not implemented.');
-  }
-
-  public decodeTag(): [bigint, unknown] {
-    const initialByte = this.data[this.position++];
-    const majorType = initialByte & BitMask.MAJOR_TYPE;
-    if (majorType != MajorType.TAG) {
-      throw new Error('Major type mismatch, expected tag.');
-    }
-    const additionalInformation = initialByte & BitMask.ADDITIONAL_INFORMATION;
-    if (additionalInformation < 24) {
-      return [BigInt(additionalInformation), this.decodeUnknownCbor()];
-    }
-    const numberOfLengthBytes = Math.pow(2, additionalInformation - 24);
-    const tag = this.decodeUint(numberOfLengthBytes);
-    return [tag, this.decodeUnknownCbor()];
-  }
-
-  private decodeUint(length: number): bigint {
-    let t = BigInt(0);
-    for (let i = 0; i < length; ++i) {
-      t = (t << 8n) | BigInt(this.data[this.position++] & 0xff);
-    }
-    return t;
   }
 }
--- tests/unit/codec/cbor/CborDecoderTest.ts
+++ tests/unit/codec/cbor/CborDecoderTest.ts
@@ -3,61 +3,72 @@ import { Base16Converter } from '../../../../src/util/Base16Converter.js';
 
 describe('Cbor decoder test', () => {
   it('Decode unsigned integer', () => {
-    expect(new CborDecoder(Base16Converter.decode('0x00')).decodeUnsignedInteger()).toEqual(0n);
-    expect(new CborDecoder(Base16Converter.decode('0x05')).decodeUnsignedInteger()).toEqual(5n);
-    expect(new CborDecoder(Base16Converter.decode('0x1B0000000105F5E0FF')).decodeUnsignedInteger()).toEqual(
+    expect(new CborDecoder(Base16Converter.decode('0x00')).readUnsignedInteger()).toEqual(0n);
+    expect(new CborDecoder(Base16Converter.decode('0x05')).readUnsignedInteger()).toEqual(5n);
+    expect(new CborDecoder(Base16Converter.decode('0x1B0000000105F5E0FF')).readUnsignedInteger()).toEqual(
       4394967295n,
     );
-    expect(() => new CborDecoder(Base16Converter.decode('0x450000000000')).decodeUnsignedInteger()).toThrow(
+    expect(() => new CborDecoder(Base16Converter.decode('0x450000000000')).readUnsignedInteger()).toThrow(
       'Major type mismatch, expected unsigned integer',
     );
   });
 
   it('Decode byte string', () => {
-    expect(new CborDecoder(Base16Converter.decode('0x40')).decodeByteString()).toEqual(new Uint8Array());
-    expect(new CborDecoder(Base16Converter.decode('0x450000000000')).decodeByteString()).toEqual(new Uint8Array(5));
+    expect(new CborDecoder(Base16Converter.decode('0x40')).readByteString()).toEqual(new Uint8Array());
+    expect(new CborDecoder(Base16Converter.decode('0x450000000000')).readByteString()).toEqual(new Uint8Array(5));
     expect(
       new CborDecoder(
         Base16Converter.decode(
           '0x58FE0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
         ),
-      ).decodeByteString(),
+      ).readByteString(),
     ).toEqual(new Uint8Array(254));
   });
 
   it('Decode text string', () => {
-    expect(new CborDecoder(Base16Converter.decode('0x60')).decodeTextString()).toEqual('');
-    expect(new CborDecoder(Base16Converter.decode('0x650A0A0A0A0A')).decodeTextString()).toEqual('\n'.repeat(5));
+    expect(new CborDecoder(Base16Converter.decode('0x60')).readTextString()).toEqual('');
+    expect(new CborDecoder(Base16Converter.decode('0x650A0A0A0A0A')).readTextString()).toEqual('\n'.repeat(5));
     expect(
       new CborDecoder(
         Base16Converter.decode('0x781B4C6F72656D20697073756D20646F6C6F722073697420616D6574'),
-      ).decodeTextString(),
+      ).readTextString(),
     ).toEqual('Lorem ipsum dolor sit amet');
     expect(
       new CborDecoder(
         Base16Converter.decode(
           '0x7901130A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A',
         ),
-      ).decodeTextString(),
+      ).readTextString(),
     ).toEqual('\n'.repeat(275));
   });
 
   it('Decode array', () => {
-    expect(new CborDecoder(Base16Converter.decode('0x834A00000000000000000000D864056474657374')).decodeArray()).toEqual(
-      [new Uint8Array(10), [100n, 5n], 'test'],
-    );
+    expect(
+      new CborDecoder(Base16Converter.decode('0x834A00000000000000000000D864056474657374'))
+        .readArray()
+        .map((element) => Base16Converter.encode(element)),
+    ).toEqual(['0x4A00000000000000000000', '0xD86405', '0x6474657374']);
   });
 
   it('Decode tag', () => {
-    expect(new CborDecoder(Base16Converter.decode('0xC1650A0A0A0A0A')).decodeTag()).toEqual([1n, '\n'.repeat(5)]);
-    expect(new CborDecoder(Base16Converter.decode('0xD819650A0A0A0A0A')).decodeTag()).toEqual([25n, '\n'.repeat(5)]);
-    expect(
-      new CborDecoder(Base16Converter.decode('0xD678190A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A')).decodeTag(),
-    ).toEqual([22n, '\n'.repeat(25)]);
-    expect(new CborDecoder(Base16Converter.decode('0xDBFFFFFFFFFFFFFFFF650A0A0A0A0A')).decodeTag()).toEqual([
-      18446744073709551615n,
-      '\n'.repeat(5),
+    expect(new CborDecoder(Base16Converter.decode('0xC1650A0A0A0A0A')).readTag()).toEqual([
+      1n,
+      Base16Converter.decode('0x650A0A0A0A0A'),
     ]);
-    expect(new CborDecoder(Base16Converter.decode('0xC1C2650A0A0A0A0A')).decodeTag()).toEqual([1n, [2n, '\n'.repeat(5)]]);
+    expect(new CborDecoder(Base16Converter.decode('0xD819650A0A0A0A0A')).readTag()).toEqual([
+      25n,
+      Base16Converter.decode('0x650A0A0A0A0A'),
+    ]);
+    expect(
+      new CborDecoder(Base16Converter.decode('0xD678190A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A')).readTag(),
+    ).toEqual([22n, Base16Converter.decode('0x78190A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A')]);
+    expect(new CborDecoder(Base16Converter.decode('0xDBFFFFFFFFFFFFFFFF650A0A0A0A0A')).readTag()).toEqual([
+      18446744073709551615n,
+      Base16Converter.decode('0x650A0A0A0A0A'),
+    ]);
+    expect(new CborDecoder(Base16Converter.decode('0xC1C2650A0A0A0A0A')).readTag()).toEqual([
+      1n,
+      Base16Converter.decode('0xC2650A0A0A0A0A'),
+    ]);
   });
 });
